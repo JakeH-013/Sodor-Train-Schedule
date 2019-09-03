577f71f2882587acaa1f49421dc62ee2756a7a60
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-09-02 23:22:54 -0400

    set code for a preset train schedule, to work with as a base

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,7 +1,22 @@
 //assumptions
 var aFrequency = 10;
-
+//first departure
 var firstTime = "7:30";
-
+//first departure converted
 var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 console.log(firstTimeConverted);
+//current time
+var currentTime = moment();
+console.log(moment(currentTime).format("hh:mm"));
+//difference between times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log(diffTime);
+//time between the train and now
+var aDifference = diffTime % aFrequency;
+console.log(aDifference);
+//time until next train
+var aMinTilTrain = aFrequency - aDifference;
+console.log(aMinTilTrain);
+//time when next train arrives
+var nextTrain = moment().add(aMinTilTrain, "minutes");
+console.log(moment(nextTrain).format("hh:mm"));
